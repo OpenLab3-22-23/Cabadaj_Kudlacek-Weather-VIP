a1f3fabad33f2230b0687fad0754e471aa7bfa90
stark501 <90406457+stark501@users.noreply.github.com>
2022-10-31 15:11:25 +0100

    Other days

--- src/main.ts
+++ src/main.ts
@@ -25,20 +25,24 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
 
 			<div class="box">
-				<h4 class="day">Wednesday</h4>
-				<h4 class="value">20°C</h4>
+				<h4 class="day_1" id="day">Wednesday</h4>
+				<img src="https://openweathermap.org/img/wn/04n.png" alt="Ikona pocasia" class="icon_1" id="icon_days" />
+				<h4 class="temp_1" id="temp">...</h4>
 			</div>
 			<div class="box">
-				<h4 class="day">Thursday</h4>
-				<h4 class="value">21°C</h4>
+				<h4 class="day_2" id="day">Thursday</h4>
+				<img src="https://openweathermap.org/img/wn/04n.png" alt="Ikona pocasia" class="icon_2" id="icon_days" />
+				<h4 class="temp_2" id="temp">...</h4>
 			</div>
 			<div class="box">
-				<h4 class="day">Friday</h4>
-				<h4 class="value">18°C</h4>
+				<h4 class="day_3" id="day">Friday</h4>
+				<img src="https://openweathermap.org/img/wn/04n.png" alt="Ikona pocasia" class="icon_3" id="icon_days" />
+				<h4 class="temp_3" id="temp">...</h4>
 			</div>
 			<div class="box">
-				<h4 class="day">Saturday</h4>
-				<h4 class="value">19°C</h4>
+				<h4 class="day_4" id="day">Saturday</h4>
+				<img src="https://openweathermap.org/img/wn/04n.png" alt="Ikona pocasia" class="icon_4" id="icon_days" />
+				<h4 class="temp_4" id="temp">...</h4>
 			</div>
 		</div>
 		
@@ -52,7 +56,7 @@ let weather = {
 	apiKey: "4ed9898ef0d4415dd8423a2a74b26ef8",
 	fetchWeather: function (city) {
 	  fetch(
-		"https://api.openweathermap.org/data/2.5/weather?q=" +
+		"https://api.openweathermap.org/data/2.5/forecast?q=" +
 		  city +
 		  "&units=metric&appid=" +
 		  this.apiKey
@@ -67,15 +71,27 @@ let weather = {
 		.then((data) => this.displayWeather(data));
 	},
 	displayWeather: function (data) {
-	  const { name } = data;
-	  const { temp } = data.main;
-	  const { speed } = data.wind;
-	  const { icon, description } = data.weather[0];
-	  document.querySelector(".city")!.innerText =   name;
-	  document.querySelector(".temp")!.innerText = temp + "°C";
-	  document.querySelector(".windspeed")!.innerText = speed + " km/h";
-	  document.querySelector(".typeOfWeather")!.innerText = description;
-	  document.querySelector(".iconweather").src ="https://openweathermap.org/img/wn/" + icon + ".png";
+	
+	  document.querySelector(".city")!.innerText = data.city.name;
+	  document.querySelector(".temp")!.innerText = data.list[0].main.temp + "°C";
+	  document.querySelector(".windspeed")!.innerText = data.list[0].wind.speed + " km/h";
+	  document.querySelector(".typeOfWeather")!.innerText = data.list[0].weather[0].description;
+	  document.querySelector(".iconweather").src ="https://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + ".png";
+
+	  document.querySelector(".day_1")!.innerText =  data.list[7].dt_txt;
+	  document.querySelector(".day_2")!.innerText =  data.list[15].dt_txt;
+	  document.querySelector(".day_3")!.innerText =  data.list[23].dt_txt;
+	  document.querySelector(".day_4")!.innerText =  data.list[31].dt_txt;
+
+	  document.querySelector(".icon_1").src ="https://openweathermap.org/img/wn/" + data.list[7].weather[0].icon + ".png";
+	  document.querySelector(".icon_2").src ="https://openweathermap.org/img/wn/" + data.list[15].weather[0].icon + ".png";
+	  document.querySelector(".icon_3").src ="https://openweathermap.org/img/wn/" + data.list[23].weather[0].icon + ".png";
+	  document.querySelector(".icon_4").src ="https://openweathermap.org/img/wn/" + data.list[31].weather[0].icon + ".png";
+
+	  document.querySelector(".temp_1")!.innerText =   data.list[7].main.temp + "°C";
+	  document.querySelector(".temp_2")!.innerText =   data.list[15].main.temp + "°C";
+	  document.querySelector(".temp_3")!.innerText =   data.list[23].main.temp + "°C";
+	  document.querySelector(".temp_4")!.innerText =   data.list[31].main.temp + "°C";
 	  
 	},
 	search: function () {
